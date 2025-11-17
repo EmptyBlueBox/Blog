e704ee1d7546c1b26bcc1e0e75dcbdd828946841
Yutong Liang <lyt0112@outlook.com>
2025-11-16 22:19:52 -0800

    [fix] style update

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -10,8 +10,9 @@ export { default as CardList } from './CardList.astro'
 export { default as Timeline } from './Timeline.astro'
 export { default as Steps } from './Steps.astro'
 
-// Simple text rerender
+// Simple text rerender and basic UI elements
 export { default as Button } from './Button.astro'
 export { default as Spoiler } from './Spoiler.astro'
 export { default as FormattedDate } from './FormattedDate.astro'
 export { default as Label } from './Label.astro'
+export { default as InfoCard } from './InfoCard.astro'
