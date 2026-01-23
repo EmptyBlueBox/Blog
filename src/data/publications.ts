c4f3e2b9bcdd360f72ff472a5871737af7407b54
Yutong Liang <lyt0112@outlook.com>
2026-01-23 13:41:50 -0800

    [feat] geather pubs

---
+++ src/data/publications.ts
@@ -0,0 +1,58 @@
+export const publicationsData = [
+  {
+    name: 'DexterCap: An Affordable and Automated System for Capturing Dexterous Hand-Object Manipulation',
+    authors: '<br/><b>Yutong Liang</b>*, Shiyi Xu*, Yulong Zhang*, Bowen Zhan, He Zhang, Libin Liu',
+    venue: 'In Submission',
+    year: '2026',
+    image: 'covers/dextercap.png',
+    details: '',
+    links: [
+      { type: 'site', href: 'https://pku-mocca.github.io/Dextercap-Page/' },
+      { type: 'arxiv', href: 'https://arxiv.org/abs/2601.05844' },
+      { type: 'github', href: 'https://github.com/PKU-MoCCA/dextercap/' },
+      { type: 'release', href: 'https://huggingface.co/datasets/pku-mocca/DexterHand/' },
+      { type: 'slide', href: '/projects/DexterCap' }
+    ]
+  },
+  {
+    name: 'GSWorld: Closed-Loop Photo-Realistic Simulation Suite for Robotic Manipulation',
+    authors:
+      '<br/>Guangqi Jiang, Haoran Chang, Ri-Zhao Qiu, <b>Yutong Liang</b>, Mazeyu Ji, Jiyue Zhu, Zhao Dong, Xueyan Zou, Xiaolong Wang',
+    venue: 'arXiv preprint 2510.20813',
+    year: '2025',
+    image: 'covers/gsworld.png',
+    details: '',
+    links: [
+      { type: 'site', href: 'https://3dgsworld.github.io' },
+      { type: 'arxiv', href: 'https://arxiv.org/abs/2510.20813' },
+      { type: 'github', href: 'https://github.com/luccachiang/GSWorld' },
+      { type: 'video', href: 'https://www.youtube.com/watch?v=uNj8RuFrhgQ' }
+    ]
+  },
+  {
+    name: 'ROBOVERSE: Towards a Unified Platform, Dataset and Benchmark for Scalable and Generalizable Robot Learning',
+    authors: '<br/>RoboVerse Team',
+    venue: 'Robotics: Science and Systems (RSS)',
+    year: '2025',
+    image: 'covers/roboverse.png',
+    details: '',
+    links: [
+      { type: 'site', href: 'https://roboverseorg.github.io' },
+      { type: 'arxiv', href: 'https://arxiv.org/abs/2504.18904' },
+      { type: 'github', href: 'https://github.com/RoboVerseOrg/RoboVerse' }
+    ]
+  },
+  {
+    name: 'SimiSketch: A Sketching Algorithm for Similarity Estimation',
+    authors:
+      '<br/>Fenghao Dong, Yang He*, <b>Yutong Liang</b>*, Zirui Liu, Yuhan Wu, Peiqing Chen, and Tong Yang',
+    venue: 'arXiv preprint 2405.19711',
+    year: '2024',
+    image: 'covers/simisketch.png',
+    details: '',
+    links: [
+      { type: 'arxiv', href: 'https://arxiv.org/abs/2405.19711' },
+      { type: 'github', href: 'https://github.com/SimiSketch/SimiSketch' }
+    ]
+  }
+]
